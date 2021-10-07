c7188590306294f0fb3060cf4cab328bef30ebd4
sajjadnayem <18103052@iubat.edu>
2021-10-07 10:37:16 +0600

    done

--- src/PSO/PSO_Scheduler.java
+++ src/PSO/PSO_Scheduler.java
@@ -1,17 +1,26 @@
 package PSO;
 
 
-import org.cloudbus.cloudsim.*;
+import java.text.DecimalFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.cloudbus.cloudsim.Cloudlet;
+import org.cloudbus.cloudsim.CloudletSchedulerSpaceShared;
+import org.cloudbus.cloudsim.Datacenter;
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.UtilizationModel;
+import org.cloudbus.cloudsim.UtilizationModelFull;
+import org.cloudbus.cloudsim.Vm;
 import org.cloudbus.cloudsim.core.CloudSim;
-import org.cloudbus.cloudsim.provisioners.BwProvisionerSimple;
-import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple;
-import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
+
 import utils.Constants;
 import utils.DatacenterCreator;
 import utils.GenerateMatrices;
-
-import java.text.DecimalFormat;
-import java.util.*;
 
 public class PSO_Scheduler {
 
--- src/SJF/SJF_Scheduler.java
+++ src/SJF/SJF_Scheduler.java
@@ -1,20 +1,23 @@
 package SJF;
 
 
-import org.cloudbus.cloudsim.*;
-import org.cloudbus.cloudsim.core.CloudSim;
-import org.cloudbus.cloudsim.provisioners.BwProvisionerSimple;
-import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple;
-import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
-import utils.Constants;
-import utils.DatacenterCreator;
-import utils.GenerateMatrices;
-
 import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
+
+import org.cloudbus.cloudsim.Cloudlet;
+import org.cloudbus.cloudsim.CloudletSchedulerSpaceShared;
+import org.cloudbus.cloudsim.Datacenter;
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.UtilizationModel;
+import org.cloudbus.cloudsim.UtilizationModelFull;
+import org.cloudbus.cloudsim.Vm;
+import org.cloudbus.cloudsim.core.CloudSim;
+
+import utils.Constants;
+import utils.DatacenterCreator;
+import utils.GenerateMatrices;
 
 public class SJF_Scheduler {
 
